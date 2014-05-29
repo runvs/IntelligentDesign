a6ae82573d71aec0353f3df71d5d3b387373505c
XzzX <xzzx@xzzx.info>
2014-05-29 12:30:08 +0200

    buggy test programm

--- Evolution.ConsoleTester/Program.cs
+++ Evolution.ConsoleTester/Program.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Evolution;
+
 namespace Evolution.ConsoleTester
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Evolution Testing Programm!");
+            World world = new World();
         }
     }
 }
