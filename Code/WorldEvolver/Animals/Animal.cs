c4094ed9139f2d1fbc8f08c9d8b622d787208d60
Thunraz <julian.dinges@gmail.com>
2014-06-18 18:11:46 +0200

    First version of Animal and AnimalProperties.

--- Code/WorldEvolver/Animals/Animal.cs
+++ Code/WorldEvolver/Animals/Animal.cs
@@ -1,11 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using JamUtilities;
+using SFML.Graphics;
 
 namespace WorldEvolver
 {
-    public class Animal
+    public class Animal : IGameObject
     {
+        private cWorld _world;
+
+        public float HealthMax { get; private set; }
+        public float HealthCurrent { get; private set; }
+        public float HealthRegeneration { get; private set; }
+        public float MoveSpeed { get; private set; }
+        public float Damage { get; private set; }
+        public float Hunger { get; private set; }
+        public float PreferredTemperature { get; private set; }
+        public float PreferredAltitude { get; private set; }
+
+        public Animal(AnimalProperties properties, cWorld world)
+        {
+            CalculateAnimalParameters(properties);
+        }
+
+        public bool IsDead()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetInput()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TimeObject timeObject)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Draw(RenderWindow rw)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CalculateAnimalParameters(AnimalProperties properties)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Code/WorldEvolver/Animals/AnimalProperties.cs
+++ Code/WorldEvolver/Animals/AnimalProperties.cs
@@ -1,11 +1,59 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+﻿
 namespace WorldEvolver
 {
     public class AnimalProperties
     {
+        /// <summary>
+        /// The stamina of an animal. Determines how much damage an animal can endure before dying.
+        /// </summary>
+        public float Stamina { get; set; }
+
+        /// <summary>
+        /// The agility of an animal. Determines how fast an animal can walk.
+        /// </summary>
+        public float Agility { get; set; }
+
+        /// <summary>
+        /// The strength of an animal. Determines how much damage an animal may deal.
+        /// </summary>
+        public float Strength { get; set; }
+
+        /// <summary>
+        /// What kind of food the animal wants to eat.
+        /// </summary>
+        public DietType Diet { get; set; }
+
+        /// <summary>
+        /// The higher this value is the more the animals tend to stick together.
+        /// </summary>
+        public float GroupBehaviour { get; set; }
+
+        /// <summary>
+        /// Which type of terrain the animal prefers.
+        /// </summary>
+        public TerrainType PreferredTerrain { get; set; }
+
+        /// <summary>
+        /// Which altitude the animal prefers.
+        /// </summary>
+        public float PreferredAltitude { get; set; }
+
+        /// <summary>
+        /// Which temperature the animal prefers.
+        /// </summary>
+        public float PreferredTemperature { get; set; }
+
+        public enum TerrainType
+        {
+            LAND,
+            WATER
+        }
+
+        public enum DietType
+        {
+            HERBIVORE,
+            CARNIVORE,
+            OMNIVORE
+        }
     }
 }
